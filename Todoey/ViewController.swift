b2abfdacca2fa0cada025f5f356b7e38229b4c8d
Glebermon <monetchikov.gleb@mail.ru>
2019-07-01 20:28:59 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Глеб on 01/07/2019.
+//  Copyright © 2019 Глеб. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
